38793276cc4a6709e9cefaa784756c4361440313
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-19 16:57:09 +0700

    Update Polimorpi.cpp

--- Polimorpi.cpp
+++ Polimorpi.cpp
@@ -2,3 +2,11 @@
 #include <string>
 using namespace std;
 
+class seseorang {
+public:
+    virtual void pesan() {};
+    // virtual void pesan(){
+    // cout << "pesan dari seseorang" endl;    
+    //}
+};
+
