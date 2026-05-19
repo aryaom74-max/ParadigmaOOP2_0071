a5170ff8d35e97f784809eaa99812d2cbf07b2a7
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-19 16:55:29 +0700

    Update Nopewaris.cpp

--- Nopewaris.cpp
+++ Nopewaris.cpp
@@ -1,31 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class baseClass final {
-public:
-    virtual void perkenalan() {
-        cout << "Hallo saya Function dari base class";
-    }
-};
-
-class baseClass1 {
-public:
-    virtual void perkenalan() {
-        cout << "Hallo saya Function dari base class";
-    }
-};
-
-class derivedClass : public baseClass1 {
-public:
-    void perkenalan() {
-        cout << "Hallo saya Function dari derived Class";
-    }
-};
-
-int main() {
-    derivedClass a;
-    a.perkenalan();
-    // a.perkenalan1(); // Baris ini di-comment atau dihapus jika perkenalan1() tidak didefinisikan di class ini
-
-    return 0;
-}
