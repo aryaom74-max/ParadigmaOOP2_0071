b16d842184f23fc251bfc37bbac8c113a33a9270
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-19 16:52:06 +0700

    Update Abstraksi.cpp

--- Abstraksi.cpp
+++ Abstraksi.cpp
@@ -1,25 +1,4 @@
 #include <iostream>
 using namespace std;
 
-class AbsraksiKlas {
-    private:
-    string x, y;
-    public:
-      // method untuk mengisi nilai
-      // private member
-      void setXY(string a, string b) {
-        x = a;
-        y = b;
-      }
-      //menampilkan nilai
-      void display() {
-        cout << "Nilai x: " << x << endl;
-        cout << "Nilai y: " << y << endl;
-      }
-    };
-    int main () {
-        AbsraksiKlas ak;
-        ak.setXY("Yogyakarta", "kampus"); 
-        ak.display();
-        return 0;
-    }
+
