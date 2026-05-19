d3b000df94de762ee9d9db5d072733dad664d113
Muh Arya Saputra <aryaom74@gmail.com>
2026-05-19 17:00:41 +0700

    Update Quizz.cpp

--- Quizz.cpp
+++ Quizz.cpp
@@ -10,3 +10,23 @@ protected:
     double saldo;
     double admin; 
 
+public:
+    // Constructor
+    RekeningBank(string nama, double sld, double adm) {
+        namaPemilik = nama;
+        saldo = sld;
+        admin = adm;
+    }
+
+    // Fungsi virtual agar bisa di-override oleh kelas turunan
+    virtual void potongAdmin() {
+        cout << "Memproses potongan admin..." << endl;
+    }
+
+    virtual void tampilkanInfo() {
+        cout << "Nama Pemilik : " << namaPemilik << endl;
+        cout << "Saldo Saat Ini: Rp " << saldo << endl;
+        cout << "-----------------------------------" << endl;
+    }
+};
+
